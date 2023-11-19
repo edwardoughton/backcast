afc3bd2406a62b54994dc22cbefe8d930a428cd1
edwardoughton <edward.oughton@gmail.com>
2023-11-19 10:19:17 -0500

    Segment osm roads into grid

--- scripts/grid.py
+++ scripts/grid.py
@@ -12,8 +12,9 @@ import pandas as pd
 import numpy as np
 import pyproj
 import geopandas as gpd
-from shapely.geometry import Polygon 
+from shapely.geometry import Polygon, Point 
 from shapely.ops import transform
+from tqdm import tqdm
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read(os.path.join(os.path.dirname(__file__),'..', 'scripts', 'script_config.ini'))
@@ -109,7 +110,7 @@ def combined_with_roads(iso3, side_length):
     grid = gpd.read_file(path_in, crs='epsg:4326')
     grid = grid.to_dict('records')
 
-    filename = 'road_network_processed.shp'
+    filename = 'gis_osm_roads_free_1.shp'
     folder = os.path.join(DATA_PROCESSED, iso3, 'infrastructure')
     path_in = os.path.join(folder, filename)
     roads = gpd.read_file(path_in, crs='epsg:4326')
@@ -143,18 +144,271 @@ def combined_with_roads(iso3, side_length):
     return
 
 
+def segment_lower_into_upper_grid(country, side_length_lower, side_length_upper):
+    """
+    Segment the lower into the upper grid. 
+
+    """
+    directory = os.path.join(DATA_PROCESSED, iso3, 'grid')
+    filename = 'grid_{}_{}_km.shp'.format(side_length_upper, side_length_upper)
+    path_in = os.path.join(directory, filename)
+    grid_upper = gpd.read_file(path_in, crs='epsg:4326')
+    grid_upper = grid_upper.to_dict('records')#[2:3]
+
+    directory = os.path.join(DATA_PROCESSED, iso3, 'grid')
+    filename = 'grid_{}_{}_km.shp'.format(side_length_lower, side_length_lower)
+    path_in = os.path.join(directory, filename)
+    grid_lower = gpd.read_file(path_in, crs='epsg:4326')
+    grid_lower = grid_lower.to_dict('records')
+
+    for tile_upper in grid_upper:
+
+        filename = '{}.shp'.format(tile_upper['GID_id'])
+        folder = os.path.join(DATA_PROCESSED, iso3, 'grid', 'grid_lower')
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        path_output = os.path.join(folder, filename)
+
+        # if os.path.exists(path_output):
+        #     continue
+
+        geom_upper = tile_upper['geometry']
+
+        output = []
+
+        for tile_lower in grid_lower:
+
+            if geom_upper.intersects(tile_lower['geometry'].representative_point()):
+                output.append({
+                    'geometry': tile_lower['geometry'],
+                    'properties':{
+                        'GID_id': tile_lower['GID_id'],
+                    }
+                })
+
+        output = gpd.GeoDataFrame.from_features(output, crs='epsg:4326')
+
+        output.to_file(path_output, crs="epsg:4326")
+
+    return
+
+
+def cut_roads_with_upper_grid(iso3, side_length_upper):
+    """
+    Cut roads with upper grid. 
+
+    """
+    filename = 'gis_osm_roads_free_1.shp'
+    folder = os.path.join(DATA_RAW, 'osm')
+    # folder = os.path.join(DATA_PROCESSED, iso3, 'infrastructure')
+    path_in = os.path.join(folder, filename)
+    roads_all = gpd.read_file(path_in, crs='epsg:4326')
+
+    folder = os.path.join(DATA_PROCESSED, iso3, 'grid', 'grid_lower')
+    filenames = os.listdir(folder)
+    for filename in tqdm(filenames):
+        
+        if not filename.endswith('.shp'):
+            continue
+        
+        folder = os.path.join(DATA_PROCESSED, iso3, 'infrastructure', 'grid_{}'.format(side_length_upper))
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        path_output = os.path.join(folder, filename)
+
+        if os.path.exists(path_output):
+            continue
+
+        print('--Working on {}'.format(filename))
+
+        folder = os.path.join(DATA_PROCESSED, iso3, 'grid', 'grid_lower')
+        path_in = os.path.join(folder, filename)
+
+        if not os.path.exists(path_in):
+            continue
+
+        grid = gpd.read_file(path_in, crs='epsg:4326')#[:5]
+        grid['col1'] = 0
+        grid = grid.dissolve("col1")
+
+        roads = gpd.overlay(roads_all, grid, how='intersection')
+
+        if len(roads) == 0:
+            continue
+
+        roads.to_file(path_output, crs="epsg:4326")
+
+    return
+
+
+def segment_roads_to_lower(iso3, side_length_lower, side_length_upper):
+    """
+    Segment road network. 
+
+    """
+    folder = os.path.join(DATA_PROCESSED, iso3, 'grid', 'grid_lower')
+    filenames = os.listdir(folder)
+    for filename in filenames:
+        
+        if not filename.endswith('.shp'):
+            continue
+        
+        directory = os.path.join(DATA_PROCESSED, iso3, 'infrastructure', 'grid_{}'.format(side_length_upper))
+        path_in = os.path.join(directory, filename)
+        if not os.path.exists(path_in):
+            continue
+        road_network = gpd.read_file(path_in, crs='epsg:4326')
+
+        directory = os.path.join(DATA_PROCESSED, iso3, 'grid', 'grid_lower')
+        path_in = os.path.join(directory, filename)
+        if not os.path.exists(path_in):
+            continue
+        grid_lower = gpd.read_file(path_in, crs='epsg:4326')
+
+        road_network = gpd.overlay(road_network, grid_lower, how='intersection', keep_geom_type=True)
+
+        grid_lower = grid_lower.to_dict('records')
+        road_network = road_network.to_dict('records')
+
+        output = []
+
+        for tile_lower in grid_lower:
+            for road_tile in road_network:
+                if tile_lower['geometry'].intersects(road_tile['geometry']):
+                    output.append({
+                        'geometry': road_tile['geometry'],
+                        'properties':{
+                            'id_upper': filename.replace('.shp',''),
+                            'id_lower': tile_lower['GID_id'],
+                            'fclass': road_tile['fclass'],
+                        }
+                    })
+
+        output = gpd.GeoDataFrame.from_features(output, crs='epsg:4326')
+
+        folder = os.path.join(DATA_PROCESSED, iso3, 'infrastructure', 'grid_{}'.format(side_length_lower))
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        path_output = os.path.join(folder, filename)
+
+        output.to_file(path_output, crs="epsg:4326")
+
+    return
+
+
+def export_road_network_metrics(country, side_length_lower):
+    """
+    Export regional metrics. 
+
+    """
+    folder = os.path.join(DATA_PROCESSED, iso3, 'infrastructure', 'grid_{}'.format(side_length_lower))
+    filenames = os.listdir(folder)
+
+    output = []
+    
+    for filename in filenames:
+        
+        if not filename.endswith('.shp'):
+            continue
+
+        print('Working on {}'.format(filename))
+
+        path_in = os.path.join(folder, filename)
+        if not os.path.exists(path_in):
+            continue
+        road_network = gpd.read_file(path_in, crs='epsg:4326')
+        road_network = road_network.to_crs(3857)
+        road_network['length_km'] = road_network['geometry'].length / 1e3
+        
+        unique_ids = road_network['id_lower'].unique()#[:1]
+
+        for unique_id in unique_ids:
+
+            subset = road_network[road_network['id_lower'] == unique_id]
+
+            subset = subset[['fclass', 'length_km']]
+            subset = round(subset.groupby('fclass').sum(), 1).reset_index()
+            subset = dict(subset.values)
+
+            if 'living_street' in subset.keys():
+                living_street = subset['living_street']
+            else:
+                living_street = 0
+
+            if 'primary' in subset.keys():
+                primary = subset['primary']
+            else:
+                primary = 0
+
+            if 'residential' in subset.keys():
+                residential = subset['residential']
+            else:
+                residential = 0
+
+            if 'secondary' in subset.keys():
+                secondary = subset['secondary']
+            else:
+                secondary = 0
+
+            if 'tertiary' in subset.keys():
+                tertiary = subset['tertiary']
+            else:
+                tertiary = 0
+
+            if 'trunk' in subset.keys():
+                trunk = subset['trunk']
+            else:
+                trunk = 0
+
+            if 'unclassified' in subset.keys():
+                unclassified = subset['unclassified']
+            else:
+                unclassified = 0
+
+            output.append({
+                'iso3': iso3,
+                'id_lower': unique_id,
+                'living_street': living_street,
+                'primary': primary,
+                'residential': residential,
+                'secondary': secondary,
+                'tertiary': tertiary,
+                'trunk': trunk,
+                'unclassified': unclassified,
+                'total': living_street + primary + residential + secondary + tertiary + trunk + unclassified
+            })
+
+    output = pd.DataFrame(output)
+
+    filename = 'road_lengths_by_region.csv'
+    folder = os.path.join(DATA_PROCESSED, iso3, 'infrastructure')
+    path_out = os.path.join(folder, filename)
+
+    output.to_csv(path_out, index=False)
+
+    return
+
+
 if __name__ == "__main__":
 
     countries = [
-        ("MEX", 10000),
+        ("MEX", 100000, 10000),
     ]
 
     for country in countries:
 
         iso3 = country[0]
-        side_length = country[1]
+        side_length_upper = country[1]
+        side_length_lower = country[2]
     
-        # ##Generate grids
-        # generate_grid(iso3, side_length) 
+        ##Generate grids
+        generate_grid(iso3, side_length_lower) 
+        generate_grid(iso3, side_length_lower) 
 
-        combined_with_roads(iso3, side_length)
+        segment_lower_into_upper_grid(iso3, side_length_lower, side_length_upper)
+
+        cut_roads_with_upper_grid(iso3, side_length_upper)
+
+        segment_roads_to_lower(iso3, side_length_lower, side_length_upper)
+
+        export_road_network_metrics(iso3, side_length_lower)
